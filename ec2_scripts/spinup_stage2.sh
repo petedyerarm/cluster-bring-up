a26f092bfac163f15bd13955a854880cb41b7271
Pete Dyer <pete.dyer@izumanetworks.com>
2024-05-20 11:54:55 +0100

    wip

--- ec2_scripts/spinup_stage2.sh
+++ ec2_scripts/spinup_stage2.sh
@@ -103,3 +103,8 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "source <(kubeadm completion bash)" >> ~/.bashrc
+source <(kubectl completion bash)
+source <(kubeadm completion bash)
